0ff7b4d394327bb9651adc9ba969b8e589185ddf
Gengo-bit <paulcorsino28@gmail.com>
2026-02-16 12:17:41 +0800

    add sorting algo

---
+++ sorting_algorithm.py
@@ -0,0 +1,40 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+
+if __name__ == '__main__':
+    sample = [64, 34, 25, 12, 22, 11, 90]
+
+    print("Original Array:", sample)
+
+    print("\nSelection Sort:", selection_sort(sample.copy()))
+    print("Insertion Sort:", insertion_sort(sample.copy()))
+    print("Quick Sort:", quick_sort(sample.copy()))
